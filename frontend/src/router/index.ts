eec3cb0908a8d740d6b5f3294f011a110f1dc893
Stefano <strozzostefano@gmail.com>
2025-03-29 21:49:23 +0100

    Fixed router error

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import PostList from '../components/PostList.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.VITE_API_BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
